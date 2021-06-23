900dacc0789453c5c8a731119247396121290398
Yuyu <ujunglim@naver.com>
2021-06-23 15:54:58 +0800

    input image

--- src/App.js
+++ src/App.js
@@ -1,49 +1,99 @@
-import { useEffect, useState } from 'react';
+import { ImageOutlined } from '@material-ui/icons';
+import { useState } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 
-function App() {
-  const [text, setText] = useState("")
-
-  const textChange = (e) => {
-    const {target: {value}} = e;
-    setText(value);
-  }
+export default function App() {
 
   const handleBold = () => {
+    // get user's selection
     let selObj = window.getSelection();
 
     if(selObj.type === 'Range') {
-      const boldText = document.createElement('b');
-      boldText.innerHTML = selObj.toString();
+      // create <b> dom
+      const boldTextDOM = document.createElement('b');
+      // get str of selection, set as <b> html
+      boldTextDOM.innerHTML = selObj.toString();
 
       const range = selObj.getRangeAt(0);
       range.deleteContents(); // Deletes selected text
-      range.insertNode(boldText);
+      range.insertNode(boldTextDOM);
+    }
+  }
+
+  const handleImage = (event) => {
+    const selObj = window.getSelection();
+
+    // Caret, Range are ok
+    if(selObj.type !== 'None') {
+      const {target: {files}} = event;
+      const theFile = files[0];
+  
+      const reader = new FileReader();
+      reader.onloadend = (finishedEvent) => {
+        const {currentTarget:{result}} = finishedEvent;
+        document.getElementById("image_file").value = null;
+        //============
+        const imgDOM = document.createElement('img');  // create <img> dom
+        imgDOM.src = result; // give file url as src
+
+        const range = selObj.getRangeAt(0);
+        range.deleteContents(); 
+        range.insertNode(imgDOM);
+        //============
+      }
+      reader.readAsDataURL(theFile);
     }
   }
 
 
-
   return (
     <>
+    <GlobalStyle />
       <h1>Yujung's Text Editor</h1>
       <button onClick={handleBold}>Bold</button>
+      <input id="image_file" type="file" accept="image/*" style={{display: "none"}} onChange={handleImage}/>
+      <label htmlFor="image_file" style={{cursor: "pointer"}}>
+        <ImageOutlined />
+      </label>
 
-      <SpanDiv>
-        <Span contentEditable={true}>sdf</Span>
-      </SpanDiv>
+      <SpanBlock>
+        <Span contentEditable={true} />
+      </SpanBlock>
     </>
   );
 }
 
-export default App;
-
 //===================== Styled Components =================
-const SpanDiv = styled.div`
+const GlobalStyle = createGlobalStyle`
+ * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    padding: 0 9rem;
+    user-select: none;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-size: 1rem;
+  }
+
+  img {
+    user-drag: none; 
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+    pointer-events: none;
+  }
+`;
+
+const SpanBlock = styled.div`
   width: 100%;
   height: 100%;
   background-color: coral;
-
 `;
 
 const Span = styled.span`
@@ -52,5 +102,6 @@ const Span = styled.span`
   width: 100%;
   height: 100%;
   background-color: pink;
+  display: block;
 `;
 
